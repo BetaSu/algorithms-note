3763269a86ce5373ce35ec003f2e3de9e34de26d
superorangecc@163.com <313439271@qq.com>
2018-01-07 22:00:55 +0800

    选择排序

---
+++ src/note/chapter2/Selection.java
@@ -0,0 +1,45 @@
+package note.chapter2;
+
+import std.In;
+import std.StdOut;
+
+/**
+ * Created by Beta_Su on 2018/1/7.
+ * 选择排序
+ */
+public class Selection {
+    public static void sort(Comparable[] a) {
+
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
